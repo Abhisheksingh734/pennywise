0fe48b35ac78712df84b41718757549d02d26832
Anisha <anishalathwalal1983@gmail.com>
2024-09-15 20:09:46 +0530

    signOut auth

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,16 +1,48 @@
 // import React from 'react'
+import { useNavigate } from "react-router-dom";
+import { auth } from "../../firebase";
 import "./styles.css";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useEffect } from "react";
+import { toast } from "react-toastify";
+import { signOut } from "firebase/auth";
 
 const Header = () => {
   function logoutFnc() {
-    alert("Logeed out");
+    try {
+      signOut(auth)
+        .then(() => {
+          // Sign-out successful.
+          toast.success("Logged out Successfully");
+          navigate("/");
+        })
+        .catch((error) => {
+          toast.error(error.message);
+
+          // An error happened.
+        });
+    } catch (e) {
+      toast.error(e.message);
+    }
   }
+
+  const [user, loading] = useAuthState(auth);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user) {
+      navigate("/dashboard");
+    }
+  }, [user, loading]);
+
   return (
     <div className="navbar">
       <p className="logo">PennyWise</p>
-      <p className="logo link" onClick={logoutFnc}>
-        Logout
-      </p>
+      {user && (
+        <p className="logo link" onClick={logoutFnc}>
+          Logout
+        </p>
+      )}
     </div>
   );
 };
