468f1b6d787dfd0ac3dbac51bf62c4b42b987025
Abhishek <abhisheksingh734@gmail.com>
2024-09-17 03:04:17 +0530

    css fix 1.1

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -7,7 +7,7 @@ function Cards({ showExpenseModal, showIncomeModal }) {
   return (
     <div>
       <Row className="my-row">
-        <Card className="my-card" title="Current Balance">
+        <Card className="my-card " title="Current Balance">
           <p>$0</p>
           <Button text={"Reset Balance"} blue={true} />
         </Card>
